0b13a43b67e668b0ca8802bff8a24dcec75a9a35
Kelsey Low <kklowlow@gmail.com>
2019-04-26 01:21:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+  end
+  
+  get '/square/:number' do
+  end
 
+  
+  
 end
